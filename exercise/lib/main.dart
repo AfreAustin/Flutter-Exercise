561a3827fb9bea08f44096e89b9eaf3e8e043459
Austin <austin.es.grimes@gmail.com>
2023-10-18 11:22:24 -0500

    finished basic format of Routine route

--- exercise/lib/main.dart
+++ exercise/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(ExerciseApp());
 
@@ -17,8 +18,12 @@ class ExerciseApp extends StatelessWidget {
   // display app within device UI
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Exercise App', // OS task switcher title
+      theme: ThemeData(
+        colorScheme: ColorScheme.dark(onPrimary: Colors.purple),
+        useMaterial3: true,
+      ),
       home: SafeArea(
         child: PageNavigation(),
       ),
@@ -49,6 +54,7 @@ class _PageNavigationState extends State<PageNavigation> {
           });
         },
         selectedIndex: currentPageIndex,
+        indicatorColor: Colors.deepPurpleAccent,
         destinations: const <Widget>[
           NavigationDestination(icon: Icon(Icons.alarm), label: "Routine"),
           NavigationDestination(icon: Icon(Icons.yard), label: "Exercises"),
@@ -81,13 +87,79 @@ List<Exercise> generateExercises() {
 }
 
 // show the set routine of a user -------------------------------------------------------------
-class RoutinePage extends StatelessWidget {
+class RoutinePage extends StatefulWidget {
   const RoutinePage({super.key});
 
   @override
+  State<RoutinePage> createState() => _RoutinePageState();
+}
+
+class _RoutinePageState extends State<RoutinePage> {
+  var f = NumberFormat('###.#%');
+  int currDay = 3;
+  int maxDay = 30;
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Routine Page"),
+    return Column(
+      children: <Widget>[
+        SizedBox(height: 20.0),
+        DecoratedBox(
+          decoration: BoxDecoration(
+            border: Border.all(width: 1, color: Colors.white),
+            color: Colors.deepOrange,
+          ),
+          child: SizedBox(
+            height: 100.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Stack(
+                  children: [
+                    Text(
+                      f.format(currDay / maxDay),
+                    ),
+                    CircularProgressIndicator(
+                      strokeCap: StrokeCap.round,
+                      backgroundColor: Colors.black,
+                      value: currDay / maxDay,
+                    ),
+                  ],
+                ),
+                Text(
+                  "Progress: ${((maxDay - currDay == 0) ? "Completed!" : "${maxDay - currDay} days left")}",
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    debugPrint("Pressed");
+                  },
+                  child: Icon(Icons.play_arrow),
+                ),
+              ],
+            ),
+          ),
+        ),
+        SizedBox(height: 20.0),
+        SizedBox(
+          height: MediaQuery.of(context).size.height - 220.0,
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.white),
+            ),
+            child: ListView.builder(
+              itemCount: 20,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  isThreeLine: true,
+                  leading: FlutterLogo(),
+                  title: Text("Exercise"),
+                  subtitle: Text("  "),
+                );
+              },
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
@@ -166,124 +238,10 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Profile Page"),
+    return Column(
+      children: const <Widget>[
+        Text("Profile Page"),
+      ],
     );
   }
 }
-
-/*
-Row(
-  mainAxisAlignment: MainAxisAlignment.end,
-  children: const <Widget>[
-    Text("List View "),
-    Icon(Icons.arrow_drop_down),
-  ],
-),
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-*/
